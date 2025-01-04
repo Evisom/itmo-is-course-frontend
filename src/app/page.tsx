23703a981618be8deccd2ffb684b0592eb794041
Dmitriy Shishmintsev <dmitriyshishmintsev@gmail.com>
2025-01-05 01:29:29 +0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <Button variant="text">Text</Button>
+    </div>
+  );
+}
