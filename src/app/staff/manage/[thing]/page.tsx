43e25c40a7d849e2b2285769e41d005a293f9510
Dmitriy Shishmintsev <dmitriyshishmintsev@gmail.com>
2025-01-05 23:49:37 +0300

    idk

--- src/app/staff/manage/[thing]/page.tsx
+++ src/app/staff/manage/[thing]/page.tsx
@@ -1,8 +1,20 @@
-const ManagePage = ({ params }) => {
+"use client";
+import { Progress } from "@/app/components/Progress";
+import { useRequireAuth } from "@/app/utils/useRequireAuth";
+import { useRouter } from "next/navigation";
+
+const ManagePage = ({ params }: { params: any }) => {
   const { thing } = params;
   const allowedParams = ["authors", "genres", "themes", "publishers"];
+  const { loading } = useRequireAuth();
+  const router = useRouter();
 
   if (!allowedParams.includes(thing)) {
+    router.back();
+  }
+
+  if (loading) {
+    return <Progress />;
   }
   return <>{thing}</>;
 };
